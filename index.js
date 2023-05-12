275fb527579ec3932fe80de7896e4c4ca0585068
Caitano <josiasmartinscaitano@gmail.com>
2023-05-12 02:04:07 -0300

    v3

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const fs = require('fs');
 const html = require('html');
 const readline = require('readline');
-
 
 const app = express();
 const port = 3001;
@@ -18,133 +17,108 @@ app.get('/', (req, res) => {
   console.log("CLIENTE")
 });
 
-
-
 app.post('/buscar-dependencia', (req, res) => {
   const pasta = req.body.pasta;
   const dependencia = req.body.dependencia;
 
-  buscarDependencia(pasta, dependencia).then((resultados) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.json(resultados);
-    console.log(resultados, "IBAG THEN");
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send('Erro ao buscar dependência');
-  });
+  const resultados = buscarDependenciaSync(pasta, dependencia);
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(resultados);
+  console.log(resultados, "IBAG THEN");
 });
 
 app.listen(port, () => {
   console.log(`Servidor escutando na porta ${port}`)
 });
 
-async function buscarDependencia(pasta, dependencia) {
-  const arquivos = await fs.promises.readdir(pasta);
-  const promessas = arquivos.map(async (arquivo) => {
+
+
+// function buscarDependenciaSync(pasta, dependencia) {
+//   const arquivos = fs.readdirSync(pasta);
+//   const resultados = [];
+
+//   for (let i = 0; i < arquivos.length; i++) {
+//     const arquivo = arquivos[i];
+//     const caminhoCompleto = path.join(pasta, arquivo);
+//     const stats = fs.statSync(caminhoCompleto);
+
+//     if (stats.isDirectory()) {
+//       const subresultados = buscarDependenciaSync(caminhoCompleto, dependencia);
+//       resultados.push(...subresultados);
+//     } else {
+//       const texto = fs.readFileSync(caminhoCompleto, { encoding: 'utf-8' });
+//       let linhas = [];
+//       let linhaAtual = 1;
+//       let ultimaPosicao = -1;
+      
+//       while (true) {
+//         const posicao = texto.indexOf(dependencia, ultimaPosicao + 1);
+//         if (posicao === -1) {
+//           break;
+//         }
+//         if (ultimaPosicao === -1) {
+//           linhas.push(linhaAtual);
+//         } else if (linhaAtual !== linhas[linhas.length - 1]) {
+//           linhas.push(linhaAtual);
+//         }
+//         ultimaPosicao = posicao;
+//         while (texto[ultimaPosicao - 1] !== '\n' && ultimaPosicao > 0) {
+//           ultimaPosicao--;
+//         }
+//         linhaAtual = texto.slice(0, ultimaPosicao).split('\n').length;
+//       }
+      
+//       if (linhas.length > 0) {
+//         const objeto = {
+//           nomeArquivo: arquivo,
+//           caminhoRelativo: path.relative(pasta, caminhoCompleto),
+//           linha: linhas.join(', '),
+//         };
+//         console.log(objeto, "OBJETO");
+//         resultados.push(objeto);
+//       }
+//     }
+//   }
+
+//   console.log(resultados, "RS");
+//   console.log(`Busca concluída na pasta ${pasta}`);
+//   return resultados;
+// }
+
+
+
+function buscarDependenciaSync(pasta, dependencia) {
+  const arquivos = fs.readdirSync(pasta);
+  const resultados = [];
+
+  for (let i = 0; i < arquivos.length; i++) {
+    const arquivo = arquivos[i];
     const caminhoCompleto = path.join(pasta, arquivo);
-    const stats = await fs.promises.stat(caminhoCompleto);
+    const stats = fs.statSync(caminhoCompleto);
 
     if (stats.isDirectory()) {
-      return buscarDependencia(caminhoCompleto, dependencia);
+      const subresultados = buscarDependenciaSync(caminhoCompleto, dependencia);
+      resultados.push(...subresultados);
     } else {
-      const leitor = readline.createInterface({
-        input: fs.createReadStream(caminhoCompleto, { encoding: 'utf-8' }),
-        crlfDelay: Infinity,
-      });
-
-      let numLinha = 1;
-      let encontrado = false;
-
-      return new Promise((resolve) => {
-        leitor.on('line', (linha) => {
-          if (linha.includes(dependencia)) {
-            encontrado = true;
-            resolve({
-              nomeArquivo: arquivo,
-              caminhoRelativo: path.relative(pasta, caminhoCompleto),
-              linha: numLinha,
-            });
-            leitor.close();
-          }
-          numLinha++;
-        });
-
-        leitor.on('close', () => {
-          if (!encontrado) {
-            resolve(null);
-          }
-        });
-
-        leitor.on('error', (err) => {
-          console.error(`Erro ao ler o arquivo ${caminhoCompleto}: ${err}`);
-          resolve(null);
-        });
-      });
+      const linhas = fs.readFileSync(caminhoCompleto, { encoding: 'utf-8' }).split('\n');
+      for (let j = 0; j < linhas.length; j++) {
+        const linha = linhas[j];
+        if (linha.includes(dependencia)) {
+          const objeto = {
+            nomeArquivo: arquivo,
+            caminhoRelativo: path.relative(pasta, caminhoCompleto),
+            linha: j + 1,
+          };
+          console.log(objeto, "OBJETO");
+          resultados.push(objeto);
+          break;
+        }
+      }
     }
-  });
+  }
 
-  const resultados = (await Promise.all(promessas)).filter(
-    (resultado) => resultado !== null
-  );
-
+  console.log(resultados, "RS");
   console.log(`Busca concluída na pasta ${pasta}`);
   return resultados;
 }
-
-
-// async function buscarDependencia(pasta, dependencia) {
-//   return new Promise((resolve, reject) => {
-//     const resultados = [];
-
-//     const percorrerPasta = (pastaAtual) => {
-//       const arquivos = fs.readdirSync(pastaAtual);
-
-//       arquivos.forEach((arquivo) => {
-//         const caminhoCompleto = path.join(pastaAtual, arquivo);
-//         const stats = fs.statSync(caminhoCompleto);
-
-//         if (stats.isDirectory()) {
-//           // se o arquivo atual for um diretório, chamar a função recursivamente para percorrer a subpasta
-//           percorrerPasta(caminhoCompleto);
-//         } else {
-//           // se o arquivo atual for um arquivo, ler o arquivo em busca da dependência
-//           const leitor = readline.createInterface({
-//             input: fs.createReadStream(caminhoCompleto, { encoding: 'utf-8' }),
-//             crlfDelay: Infinity
-//           });
-
-//           let numLinha = 1;
-
-//           leitor.on('line', (linha) => {
-//             if (linha.includes(dependencia)) {
-//               console.log("INCLUI")
-//               resultados.push({
-//                 nomeArquivo: arquivo,
-//                 caminhoRelativo: path.relative(pasta, caminhoCompleto),
-//                 linha: numLinha,
-//               });
-//               console.log(resultados, "RESULTADO")
-//             }
-//             numLinha++;
-//           });
-
-//           leitor.on('close', () => {
-//             console.log(`Busca concluída no arquivo ${caminhoCompleto}`);
-//           });
-
-//           leitor.on('error', (err) => {
-//             console.error(`Erro ao ler o arquivo ${caminhoCompleto}: ${err}`);
-//           });
-//         }
-//       });
-//     };
-
-//     console.log(pasta, "PASTAS")
-
-//     percorrerPasta(pasta);
-
-//     console.log(`Busca concluída na pasta ${pasta}`);
-//     resolve(resultados);
-//   });
-// }
-
